3977b4aaf2ba3ea1a62654d40d15c9d0a3ea638f
laseredface <122493759+pf274@users.noreply.github.com>
2023-01-21 19:54:32 +0000

    added touch recognition and pen sensitivity

--- Pages/Draw/draw.js
+++ Pages/Draw/draw.js
@@ -1,6 +1,8 @@
 function calculateMouseCoords(event, canvas) {
+  const clientX = event.clientX || event.touches[0].clientX;
+  const clientY = event.clientY || event.touches[0].clientY;
   let rect = canvas.getBoundingClientRect();
-  return [event.clientX - rect.left, event.clientY - rect.top];
+  return [clientX - rect.left, clientY - rect.top];
 }
 
 function setPrimaryButtonState(event) { // since it's possible to click out of the canvas and still have mouseDown set to more than 0, double check!
@@ -18,33 +20,59 @@ window.onload = function() {
 
   context.fillStyle = "#FFFFFF";
   context.fillRect(0, 0, canvas.width, canvas.height);
-
-  
-  canvas.addEventListener('mousedown', function(event) {
+  function startDraw(event) {
     prevCoords = calculateMouseCoords(event, canvas);
     mouseDown = 1;
-  });
+  }
 
-  canvas.addEventListener('mouseup', function(event) {
-    prevCoords = calculateMouseCoords(event, canvas);
-    mouseDown = 0;
-  });
-
-  canvas.addEventListener('mousemove', function(event) {
+  function continueDraw(event) {
+    console.log("yep!");
     primaryMouseButtonDown = setPrimaryButtonState(event);
-    if (mouseDown > 1 && primaryMouseButtonDown) {
+    if (mouseDown > 1 && (event?.touches || primaryMouseButtonDown)) {
       newCoords = calculateMouseCoords(event, canvas);
       context = canvas.getContext("2d");
+      let thickness = 4;
+      if (event?.targetTouches) {
+        thickness = 0.5 * thickness + 1.5 * thickness * event?.targetTouches[0].force;
+      }
+      context.lineWidth = thickness;
+      context.lineCap = "round";
       context.fillStyle = "#000000";
       context.moveTo(...prevCoords);
       context.lineTo(...newCoords);
       context.stroke();
+      context.beginPath();
       prevCoords = [...newCoords];
     } else if (mouseDown == 1) {
       prevCoords = calculateMouseCoords(event, canvas);
       mouseDown++;
     }
-  })
+  }
+  function endDraw(event) {
+    mouseDown = 0;
+  }
+
+  canvas.addEventListener('mousedown', function(event) {
+    startDraw(event);
+  });
+  canvas.addEventListener('touchstart', function(event) {
+    startDraw(event);
+  });
+
+  canvas.addEventListener('mouseup', function(event) {
+    endDraw(event);
+  });
+  canvas.addEventListener('touchend', function(event) {
+    endDraw(event);
+  });
+
+  canvas.addEventListener('mousemove', function(event) {
+    continueDraw(event);
+  });
+
+  canvas.addEventListener('touchmove', function(event) {
+    continueDraw(event);
+  });
 }
 
 function clearCanvas() {
