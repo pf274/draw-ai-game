214b1bdc17ea9fa8f0021c73ae0782326bbb7f90
pf274 <peterfullmer33@gmail.com>
2023-03-22 19:21:35 -0600

    Incorporated express and mongodb

--- index.js
+++ index.js
@@ -1,48 +1,153 @@
 const express = require('express');
-const cookieParser = require('cookie-parser');
+const {MongoClient} = require("mongodb");
 const app = express();
 
-// Third party middleware - Cookies
-app.use(cookieParser());
 
-// app.post('/cookie/:name/:value', (req, res, next) => {
-//   res.cookie(req.params.name, req.params.value);
-//   res.send({ cookie: `${req.params.name}:${req.params.value}` });
-// });
+// ------------- Express -------------
 
-// app.get('/cookie', (req, res, next) => {
-//   res.send({ cookie: req.cookies });
-// });
 
-// Creating your own middleware - logging
-app.use((req, res, next) => {
-  console.log(req.originalUrl);
-  next();
-});
-
-// Built in middleware - Static file hosting
+// Static file hosting
 app.use(express.static('public'));
+app.use(express.json());
 
-// Routing middleware
-app.get('/store/:storeName', (req, res) => {
-  res.send({ name: req.params.storeName });
+// APIs
+const apiRouter = express.Router();
+app.use(`/api`, apiRouter);
+
+apiRouter.get('/games/list', async (req, res) => {
+  let all_games = await getGames();
+  res.send(all_games);
 });
 
-app.put('/st*/:storeName', (req, res) => res.send({ update: req.params.storeName }));
-
-app.delete(/\/store\/(.+)/, (req, res) => res.send({ delete: req.params[0] }));
-
-// Error middleware
-app.get('/error', (req, res, next) => {
-  throw new Error('Trouble in river city');
+apiRouter.get('/games/:game', async (req, res) => {
+  let game = await getGame(req.params.game);
+  res.send(game);
 });
 
-app.use(function (err, req, res, next) {
-  res.status(500).send({ type: err.name, message: err.message });
+apiRouter.delete('/games/clear', async (req, res) => {
+  let cleared_all = await clearAllGames();
+  res.send(cleared_all);
+})
+
+apiRouter.post('/games/host', async (req, res) => {
+  let game_info = req.body;
+  let response = await hostGame(game_info);
+  res.send(response);
 });
+
+apiRouter.post('/games/start/:game_id', async (req, res) => {
+  let response = await startGame(req.params.game_id);
+  res.send(response);
+});
+
+apiRouter.get('/users/list', async (req, res) => {
+  let users = await getUsers();
+  res.send(users);
+});
+
+apiRouter.post('/users/register', async (req, res) => {
+  let user_exists = await getUser(req.body.username);
+  if (!user_exists) {
+    await userCollection.insertOne(req.body);
+    return true;
+  }
+  return false;
+});
+
+apiRouter.get('/users/login/:username/:password', async (req, res) => {
+  let user_info = await getUser(req.params.username);
+  if (user_info) {
+    let password_matches = (user_info.password == req.params.password);
+    if (password_matches) {
+      res.send({status: "successful"});
+    } else {
+      res.send({status: "invalid password"});
+    }
+  } else {
+    res.send({status: "invalid username"});
+  }
+});
+
+async function clearAllGames() {
+  try {
+    await gameCollection.deleteMany({});
+    return true;
+  } catch (err) {
+    console.log(err);
+    return false;
+  }
+
+}
+
+async function startGame(game_id) {
+  try {
+    userCollection.updateOne({id: game_id}, {$set: {status: "started"}});
+    return true;
+  } catch (err) {
+    console.log(err);
+    return false;
+  }
+
+}
+async function getUser(username) {
+  let cursor = userCollection.find({username: username});
+  return cursor.toArray().then(results => results.length > 0 ? results[0] : false);
+}
+
+async function getUsers() {
+  let cursor = userCollection.find();
+  return cursor.toArray();
+}
+
+async function hostGame(game_info) {
+  let game_exists = await getGame(game_info.id);
+  if (!game_exists) {
+    await gameCollection.insertOne(game_info);
+    return true;
+  }
+  return false;
+}
+
+async function endGame(game_info) {
+  let game_exists = await getGame(game_info.id);
+  if (game_exists) {
+    await gameCollection.deleteOne({id: game_info});
+    return true;
+  }
+  return false;
+}
+
+async function getGames() {
+  let cursor = gameCollection.find();
+  return cursor.toArray();
+}
+
+async function getGame(game_id) {
+  let cursor = gameCollection.find({id: game_id});
+  return cursor.toArray().then(results => results.length > 0 ? results[0] : false);
+}
+
+
 
 // Listening to a network port
-const port = 8080;
+const port = 4000;
 app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
+
+// ------------- MongoDB -------------
+
+let gameCollection;
+let userCollection;
+const username = process.env.MONGOUSER;
+const password = process.env.MONGOPASSWORD;
+const hostname = process.env.MONGOHOSTNAME;
+
+async function dbSetup() {
+  const url = `mongodb+srv://${username}:${password}@${hostname}`;
+  const client = new MongoClient(url);
+  gameCollection = client.db('startup').collection('games');
+  userCollection = client.db('startup').collection('users');
+}
+
+dbSetup();
--- public/index.js
+++ public/index.js
@@ -6,16 +6,15 @@ function generateCode() {
 }
 
 // initialization
-$(document).ready(function () {
+$(document).ready(async function () {
     let loggedIn = false;
-    localStorage.removeItem('game');
     if (localStorage.getItem("user_data")) {
         let user_data = JSON.parse(localStorage.getItem("user_data"));
-        if (user_data?.username in users) {
-            if (users[user_data?.username]?.password == user_data?.password) {
-                loggedIn = true;
-            }
+        let verified = await fetch(`/api/users/login/${user_data?.username}/${user_data?.password}`).then(response => response.json());
+        if (verified.status != "successful") {
+            localStorage.removeItem("user_data");
         }
+        loggedIn = (verified.status == "successful");
     }
     if (loggedIn) {
         let user_data = JSON.parse(localStorage.getItem("user_data"));
@@ -48,17 +47,21 @@ startCredentialsButton.addEventListener('click', () => {
 
 // logging in
 let loginButton = document.getElementById("LoginSubmitButton");
-loginButton.addEventListener('click', () => {
+loginButton.addEventListener('click', async () => {
     let usernameField = document.getElementById("LoginUsernameField");
     let passwordField = document.getElementById("LoginPasswordField");
 
     let username = usernameField.value;
     let password = passwordField.value;
-
-    if (username in users) {
-        if (users[username]?.password === password) {
-             // console.log("Login Successful!");
-            localStorage.setItem("user_data", JSON.stringify(users[username]));
+    let users = await fetch('/api/users/list').then(response => response.json());
+    let users_map = {};
+    for (const user of users) {
+        users_map[user.username] = user;
+    }
+    if (username in users_map) {
+        if (users_map[username]?.password === password) {
+             console.log("Login Successful!");
+            localStorage.setItem("user_data", JSON.stringify(users_map[username]));
             $('#loginModal').modal('hide');
             $('.loggedIn').css("display", "block");
             $('.noCredentials').css("display", "none");
@@ -66,13 +69,13 @@ loginButton.addEventListener('click', () => {
             lobbyTitle.innerText = `Welcome, ${username}!`;
             $("#loginCredentialsAlert").hide();
         } else {
-             // console.log("Invalid Password, scrub!");
+             console.log("Invalid Password, scrub!");
             let alert = document.getElementById('loginCredentialsAlert');
             alert.innerHTML = "Invalid password. Please try again.";
             $("#loginCredentialsAlert").show();
         }
     } else {
-         // console.log("unrecognized user!");
+        console.log("unrecognized user!");
         let alert = document.getElementById('loginCredentialsAlert');
         alert.innerHTML = "Invalid username. Please try again.";
         $("#loginCredentialsAlert").show();
@@ -81,7 +84,7 @@ loginButton.addEventListener('click', () => {
 
 // signing up
 let signupButton = document.getElementById("SignupSubmitButton");
-signupButton.addEventListener('click', () => {
+signupButton.addEventListener('click', async () => {
     let usernameField = document.getElementById("SignupUsernameField");
     let passwordField = document.getElementById("SignupPasswordField");
     let reenterPasswordField = document.getElementById("SignupReenterPasswordField");
@@ -90,19 +93,31 @@ signupButton.addEventListener('click', () => {
     let password = passwordField.value;
     let reenteredPassword = reenterPasswordField.value;
 
-    if (!(username in users)) {
+    let users = await fetch('/api/users/list').then(response => response.json());
+    let users_map = {};
+    for (const user of users) {
+        users_map[user.username] = user;
+    }
+
+    if (!(username in users_map)) {
         if (password.length > 0) {
             if (password === reenteredPassword) {
-                users[username] = {
-                    username: username,
-                    password: password,
-                };
-                 // console.log("Signup successful!");
+                let response = await fetch(`api/users/register`, {
+                    method: "POST",
+                    headers: {'Content-Type': 'application/json'},
+                    body: JSON.stringify({
+                        username: username,
+                        password: password,
+                    })
+                });
+                console.log(response);
+                console.log("Signup successful!");
+                // TODO: SKIP SIGNING IN WHEN REGISTERED
                 $("#signupModal").modal("hide");
                 $("#loginModal").modal("show");
                 $("#signupCredentialsAlert").hide();
             } else {
-                 // console.log("Passwords do not match!");
+                 console.log("Passwords do not match!");
                 let alert = document.getElementById('signupCredentialsAlert');
                 alert.innerHTML = "Passwords do not match. Please try again.";
                 $("#signupCredentialsAlert").show();
@@ -149,29 +164,46 @@ joinGameButton.addEventListener('click', () => {
 
 // Hosting a game
 let hostGameModalButton = document.getElementById("hostGameModalButton");
-hostGameModalButton.addEventListener('click', () => {
+hostGameModalButton.addEventListener('click', async () => {
+    let deleted = await fetch("api/games/clear", {method: "DELETE"}).then(response => response.json());
     let generatedCodeElement = document.getElementById("generatedCode");
     let generatedCode = generateCode(); // Generates a random game code four characters long, using chars 0-9 and A-Z.
-    while (generatedCode in games) {
+
+    let all_games = await fetch("api/games/list").then(response => response.json());
+    console.log(all_games);
+    let games_map = {};
+    for (const game of all_games) {
+        games_map[game.id] = game;
+    }
+    while (generatedCode in games_map) {
         generatedCode = generateCode();
     }
-    let username = JSON.parse(localStorage.getItem("user_data")).username
-    games[generatedCode] = {
-        game_code: generatedCode,
-        host: username,
-        participants: {
-            [username]: {},
-        }, // allow for three more participants maximum
-        game_type: "hosted",
-    }
+    let username = JSON.parse(localStorage.getItem("user_data")).username;
+    let hosted_game = await fetch("/api/games/host", {
+        method: "POST",
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+            id: generatedCode,
+            host: username,
+            participants: [],
+            status: "starting",
+        })
+    }).then(response => {
+        if (response.ok) return response.json();
+    });
+    console.log(hosted_game);
     generatedCodeElement.innerText = generatedCode;
 });
 
 // start hosted game
 let startHostedGameButton = document.getElementById("startHostedGameButton");
-startHostedGameButton.addEventListener('click', () => {
+startHostedGameButton.addEventListener('click', async () => {
     let generatedCodeElement = document.getElementById("generatedCode");
-    localStorage.setItem("game", JSON.stringify(games[generatedCodeElement.innerText]));
+    let game_id = generatedCodeElement.innerText;
+    // let username = JSON.parse(localStorage.getItem("user_data")).username;
+    let started_game = await fetch(`/api/games/start/${game_id}`, {method: "POST"}).then(response => response.json());
+    let game_info = await fetch(`api/games/${game_id}`).then(response => response.json());
+    localStorage.setItem("game", JSON.stringify(game_info));
     // TODO: Tell other players' webpages to start game.
     window.location.href = "./Pages/PartyDraw/draw.html";
 });
