c2e7081bf889b93c82711fd4c218b763dbeb237c
pf274 <peterfullmer33@gmail.com>
2023-03-29 09:20:37 -0600

    Added drawing functionality

--- src/Components/DrawPage/DrawingCanvas.jsx
+++ src/Components/DrawPage/DrawingCanvas.jsx
@@ -2,7 +2,10 @@ import './DrawingCanvas.css';
 import {useEffect, useRef} from 'react';
 function DrawingCanvas() {
     const resizing = useRef(false);
+    let drawing = useRef(false);
+    let prevCoords = useRef([0, 0]);
     let resizeTimeout = useRef();
+    let tap = useRef(false);
 
     function finishResize() {
         let canvas = document.getElementById("drawingCanvas");
@@ -20,12 +23,92 @@ function DrawingCanvas() {
         clearTimeout(resizeTimeout);
         resizeTimeout = setTimeout(finishResize, 100);
     }
+    function startDraw(event) {
+        console.log("start");
+        drawing = true;
+        const drawingCanvas = document.getElementById("drawingCanvas");
+        const colorPicker = document.getElementById("colorPicker");
+        prevCoords = calculateMouseCoords(event, drawingCanvas);
+        drawingCanvas.setAttribute("penColor", colorPicker?.value || "#000000");
+        tap = true;
+    }
+    function continueDraw(event) {
+        if (drawing) {
+            console.log("continue...");
+            tap = false;
+            const drawingCanvas = document.getElementById("drawingCanvas");
+            const thicknessSlider = document.getElementById("thicknessSlider");
+            let newCoords = calculateMouseCoords(event, drawingCanvas);
+            let context = drawingCanvas.getContext("2d");
+            let thickness = 16 * (thicknessSlider?.value || 1) * Math.min((window.innerWidth / 650), (window.innerHeight / 850));
+            if (event?.targetTouches) {
+                if (event.targetTouches.length > 0) {
+                    thickness = 0.5 * thickness + (1.5 * thickness * event?.targetTouches[0].force);
+                }
+            }
+            context.lineWidth = thickness;
+            context.lineCap = "round";
+            context.strokeStyle = drawingCanvas.getAttribute("penColor");
+            context.moveTo(...prevCoords);
+            context.lineTo(...newCoords);
+            context.stroke();
+            context.beginPath();
+            prevCoords = [...newCoords];
+        }
+    }
+    function endDraw(event) {
+        console.log("end");
+        drawing = false;
+        if (tap) {
+            const drawingCanvas = document.getElementById("drawingCanvas");
+            const thicknessSlider = document.getElementById("thicknessSlider");
+            let newCoords = calculateMouseCoords(event, drawingCanvas);
+            tap = false;
+            let thickness = 16 * (thicknessSlider.value || 1) * Math.min((window.innerWidth / 650), (window.innerHeight / 850));
+            if (event?.targetTouches) {
+                if (event.targetTouches.length > 0) {
+                    thickness = 0.5 * thickness + 1.5 * thickness * event?.targetTouches[0].force;
+                }        
+            }
+            let context = drawingCanvas.getContext("2d");
+            context.lineWidth = thickness;
+            context.lineCap = "round";
+            context.strokeStyle = drawingCanvas.getAttribute("penColor");
+            context.moveTo(...newCoords);
+            context.lineTo(newCoords[0], newCoords[1] + 1);
+            context.stroke();
+            context.beginPath();
+        }
+    }
+    function calculateMouseCoords(event, canvas) {
+        const clientX = event?.clientX || event.touches[0]?.clientX;
+        const clientY = event?.clientY || event.touches[0]?.clientY;
+        let rect = canvas.getBoundingClientRect();
+        return [clientX - rect.left, clientY - rect.top];
+    }
     useEffect(() => {
-        // when page loads, set an event listener
+        const drawingCanvas = document.getElementById("drawingCanvas");
+        // when page loads, set event listeners
         window.addEventListener('resize', startResize);
+        window.addEventListener('mousedown', function (event) {startDraw(event);});
+        window.addEventListener('touchstart', function (event) {startDraw(event);});
+        window.addEventListener('mousemove', function (event) {continueDraw(event);});
+        window.addEventListener('touchmove', function (event) {continueDraw(event);});
+        window.addEventListener('mouseup', function (event) {endDraw(event);});
+        window.addEventListener('touchend', function (event) {endDraw(event);});
+        // and initialize the canvas size
         finishResize();
+        drawing = false;
+        tap = false;
         return () => {
+            // when the elements are unloaded, remove the event listeners
             window.removeEventListener('resize', startResize);
+            window.removeEventListener('mousedown', function (event) {startDraw(event);});
+            window.removeEventListener('touchstart', function (event) {startDraw(event);});
+            window.removeEventListener('mousemove', function (event) {continueDraw(event);});
+            window.removeEventListener('touchmove', function (event) {continueDraw(event);});
+            window.removeEventListener('mouseup', function (event) {endDraw(event);});
+            window.removeEventListener('touchend', function (event) {endDraw(event);});
         }
     }, [])
     return (
--- src/Components/DrawPage/Toolbar.jsx
+++ src/Components/DrawPage/Toolbar.jsx
@@ -3,10 +3,16 @@ import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import FormRange from 'react-bootstrap/FormRange';
 function Toolbar() {
+    function clearCanvas() {
+        const drawingCanvas = document.getElementById("drawingCanvas");
+        let context = drawingCanvas.getContext("2d");
+        context.fillStyle = "rgba(255, 255, 255, 1)";
+        context.clearRect(0, 0, drawingCanvas.width, drawingCanvas.height);
+    }
     return (
         <Card id="Toolbar">
             <Button className="singleplayer">New Prompt</Button>
-            <Button className="singleplayer">Clear Canvas</Button>
+            <Button className="singleplayer" onClick={clearCanvas}>Clear Canvas</Button>
             <Button className="singleplayer">Guess</Button>
             <FormRange className="singleplayer" min="0.5" max="3" step="0.1" id="thicknessSlider"></FormRange>
             <input className="singleplayer" type="color" id="colorPicker" />
